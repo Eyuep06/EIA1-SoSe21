23264403c1c262322aee685a53780d05416ab29f
Eyuep06 <80841485+Eyuep06@users.noreply.github.com>
2021-05-20 11:37:38 +0200

    check_funktion_gemacht

--- Aufgabe_09/script.js
+++ Aufgabe_09/script.js
@@ -1,16 +1,27 @@
 var aufgabe9;
 (function (aufgabe9) {
     var benutzereingabe = document.querySelector("#addTask");
+    var taskZahler = 1;
+    var notDoneyet = document.querySelector(".fa-circle");
+    var taskDone = document.querySelector(".fa-check-circle");
     benutzereingabe.addEventListener("keypress", function (keyboardEvent) {
         if (keyboardEvent.key == "Enter") {
             var newTask = document.createElement("div");
             newTask.className = "task1";
             document.querySelector(".furtherTasks").appendChild(newTask).innerHTML = benutzereingabe.value;
+            taskZahler = taskZahler + 1;
+            document.querySelector("h3").innerHTML = taskZahler + " in total";
         }
     });
-    /* if (keyboardEvent.key == "Enter") {
-       let newTask: HTMLDivElement = document.createElement("div");
-       newTask.className = "task1";
-       document.querySelector(".task1").appendChild(newTask);*/
+    function changeClasses(firsHTMLElement, secondHTMLElement) {
+        firsHTMLElement.classList.add("isHidden");
+        secondHTMLElement.classList.remove("isHidden");
+    }
+    notDoneyet.addEventListener("click", function () {
+        changeClasses(this, taskDone);
+    });
+    taskDone.addEventListener("click", function () {
+        changeClasses(this, notDoneyet);
+    });
 })(aufgabe9 || (aufgabe9 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe_09/script.ts
+++ Aufgabe_09/script.ts
@@ -2,7 +2,9 @@ namespace aufgabe9 {
 
 
   let benutzereingabe: HTMLInputElement = document.querySelector("#addTask") as HTMLInputElement;
-  
+  let taskZahler: number = 1;
+  let notDoneyet: HTMLElement = document.querySelector(".fa-circle");
+  let taskDone: HTMLElement = document.querySelector(".fa-check-circle");
 
 
 
@@ -15,17 +17,29 @@ namespace aufgabe9 {
       let newTask: HTMLDivElement = document.createElement("div");
       newTask.className = "task1";
       document.querySelector(".furtherTasks").appendChild(newTask).innerHTML = benutzereingabe.value;
-
+      taskZahler = taskZahler + 1;
+      document.querySelector("h3").innerHTML = taskZahler + " in total";
     }
 
 
   });
 
 
-  /* if (keyboardEvent.key == "Enter") {
-     let newTask: HTMLDivElement = document.createElement("div");
-     newTask.className = "task1";
-     document.querySelector(".task1").appendChild(newTask);*/
+  function changeClasses(firsHTMLElement: HTMLElement, secondHTMLElement: HTMLElement): void {
+    firsHTMLElement.classList.add("isHidden");
+    secondHTMLElement.classList.remove("isHidden");
 
+
+  }
+
+
+  notDoneyet.addEventListener("click", function (): void {
+    changeClasses(this, taskDone);
+  });
+
+  taskDone.addEventListener("click", function (): void {
+    changeClasses(this, notDoneyet);
+
+  });
 
 }
